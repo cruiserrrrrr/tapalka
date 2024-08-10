493b67ba67c33d00bf9fa1f8bc70f47bfceafb1a
cruiserrrrrr <cruiserrrrrr@gmail.com>
2024-08-10 12:52:09 +0300

    попытка получения userID

--- src/components/energy-counter/energy-counter.tsx
+++ src/components/energy-counter/energy-counter.tsx
@@ -39,7 +39,7 @@ const EnergyCounter = (props: IEnergyCounter) => {
 
   return (
     <div className={styles.wrap}>
-      <p className={styles.text}>Your Energy: {energyPercent}% id {userId}</p>
+      <p className={styles.text}>Your Energy: {energyPercent}% id {window.location.search}</p>
       <div className={styles.container}>
         <p className={styles.text}>{defaultEnergy}</p>
         <div
